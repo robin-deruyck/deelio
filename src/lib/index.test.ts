dbb2e80f48d86029c65c070259ed8c14c6a1a159
robin-deruyck <robin.2ruyck@gmail.com>
2024-03-10 22:14:06 +0100

    chore: init project

---
+++ src/lib/index.test.ts
@@ -0,0 +1,8 @@
+import {describe, expect, it} from "vitest";
+
+describe("ouioui", () => {
+
+	it('should ', () => {
+		expect(true).toBeTruthy()
+	});
+})
---
+++ vitest.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vitest/config'
+import path from 'path'
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@/*': path.resolve(__dirname, './src')
+    }
+  },
+  test: {
+    globals: true,
+    include: ['src/**/*.test.ts'],
+    reporters: ['default', 'html'],
+    outputFile: './test-result/test-report.html',
+    coverage: {
+      reporter: ['html'],
+    }
+  }
+})
