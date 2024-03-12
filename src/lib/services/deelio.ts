f9be7946da6e0ccab9b99f9ba5aa317ffb39146f
robin-deruyck <robin.2ruyck@gmail.com>
2024-03-12 07:17:39 +0100

    chore: setup tsup and npm publishing config

--- src/lib/services/deelio.ts
+++ src/lib/services/deelio.ts
@@ -1,8 +1,11 @@
-import { DeelioConfig, validateModelsType, applyRouter, buildRouter } from '@/lib'
+import { DeelioConfig} from '@/lib'
+import { applyRouter, buildRouter } from '@/lib/services/builder'
+import { validateModelsType } from '@/lib/services/validate'
 
 export const deelio = (options: DeelioConfig) => {
   validateModelsType(options?.models ?? [])
   const router = buildRouter(options.models)
+
   return {
     registerDeelioRouter: (app: any) => {
       applyRouter(router, app)
